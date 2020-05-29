5c5effc4189c3bbacb5d9d7074d1f1d4ce05536a
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-29 13:04:07 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -26,7 +26,7 @@ def fourth_challenge
         :attitude => 'confused'
         
       },{
-        :name => 'Mercutio',
+        :name => 'Nurse',
         :age => '18',
         :attitude => 'hot-headed'
       }]
